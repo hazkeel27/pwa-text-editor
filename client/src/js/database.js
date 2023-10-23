865d13dd252edd416e15994364da1de1da223d59
Hazkeel Qureshi <hazkeel27@gmail.com>
2023-10-23 09:14:40 -0400

    dist created

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -27,7 +27,7 @@ export const getDb = async () => {
   const jateDb = await openDB('jate', 1);
   const tx = jateDb.transaction('jate', 'readonly');
   const store = tx.objectStore('jate');
-  const request = store.getAll();
+  const request = store.get(1);
   const result = await request;
   console.log('result.value', result);
   return result;
